710e277f4a4c19588b9d743a7742fb628a46e79c
Jacob Kelleran <jacob.kelleran@outlook.com>
2026-02-08 12:40:15 -0800

    Added state encoding and decoding

--- Assets/Scripts/Interactables/Door.cs
+++ Assets/Scripts/Interactables/Door.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
 using UnityEngine.Events;
+using System.Collections.Generic;
 
-public class Door : MonoBehaviour
+public class Door : StatefulInteractable
 {
     public enum DoorState
     {
@@ -10,6 +11,8 @@ public class Door : MonoBehaviour
         Locked
     }
 
+    private static readonly string StateKey = "DoorState";
+
     public UnityEvent OnOpen;
     public UnityEvent OnClose;
     public UnityEvent OnLock;
@@ -17,12 +20,33 @@ public class Door : MonoBehaviour
 
     public DoorState State { get; private set; }
 
-    void Start()
+    [SerializeField] private DoorState initialState = DoorState.Closed;
+
+    void Awake()
     {
         OnOpen ??= new UnityEvent();
         OnClose ??= new UnityEvent();
         OnLock ??= new UnityEvent();
         OnUnlock ??= new UnityEvent();
+
+        State = initialState;
+
+        StateRegistry.Instance.Register(this);
+    }
+
+    public override Dictionary<string, object> GetState()
+    {
+        base.SetValue(StateKey, State.ToString());
+        return base.GetState();
+    }
+
+    public override void SetState(Dictionary<string, object> newState)
+    {
+        base.SetState(newState);
+        if (System.Enum.TryParse(base.GetValue<string>(StateKey), out DoorState parsedState))
+        {
+            State = parsedState;
+        }
     }
 
     public void TryOpen()
